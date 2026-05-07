7326e56995ba647de32886dcb265465bea82ef01
ViaX: Trace <yankeyank90@gmail.com>
2026-05-06 22:39:20 -0300

    Add lib/screens/home_screen.dart

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,142 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import '../theme/app_theme.dart';
+import '../providers/settings_provider.dart';
+import '../providers/character_provider.dart';
+import 'chat_screen.dart';
+import 'characters_screen.dart';
+import 'settings_screen.dart';
+
+class HomeScreen extends ConsumerStatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  int _tab = 0;
+
+  static const _screens = [
+    _ChatTab(),
+    _CharactersTab(),
+    _SettingsTab(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppTheme.background,
+      body: IndexedStack(
+        index: _tab,
+        children: _screens,
+      ),
+      bottomNavigationBar: _BottomBar(
+        current: _tab,
+        onTap: (i) => setState(() => _tab = i),
+      ),
+    );
+  }
+}
+
+class _ChatTab extends StatelessWidget {
+  const _ChatTab();
+  @override
+  Widget build(BuildContext context) => const ChatScreen();
+}
+
+class _CharactersTab extends StatelessWidget {
+  const _CharactersTab();
+  @override
+  Widget build(BuildContext context) => const CharactersScreen();
+}
+
+class _SettingsTab extends StatelessWidget {
+  const _SettingsTab();
+  @override
+  Widget build(BuildContext context) => const SettingsScreen();
+}
+
+class _BottomBar extends StatelessWidget {
+  final int current;
+  final void Function(int) onTap;
+
+  const _BottomBar({required this.current, required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        color: AppTheme.surface,
+        border: Border(top: BorderSide(color: AppTheme.border, width: 1)),
+      ),
+      child: SafeArea(
+        top: false,
+        child: SizedBox(
+          height: 56,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              _NavItem(icon: Icons.chat_bubble_outline_rounded, activeIcon: Icons.chat_bubble_rounded, label: 'Chat', index: 0, current: current, onTap: onTap),
+              _NavItem(icon: Icons.people_outline_rounded, activeIcon: Icons.people_rounded, label: 'Characters', index: 1, current: current, onTap: onTap),
+              _NavItem(icon: Icons.settings_outlined, activeIcon: Icons.settings_rounded, label: 'Settings', index: 2, current: current, onTap: onTap),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _NavItem extends StatelessWidget {
+  final IconData icon;
+  final IconData activeIcon;
+  final String label;
+  final int index;
+  final int current;
+  final void Function(int) onTap;
+
+  const _NavItem({
+    required this.icon,
+    required this.activeIcon,
+    required this.label,
+    required this.index,
+    required this.current,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final isActive = current == index;
+    return GestureDetector(
+      onTap: () => onTap(index),
+      behavior: HitTestBehavior.opaque,
+      child: SizedBox(
+        width: 80,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedSwitcher(
+              duration: const Duration(milliseconds: 200),
+              child: Icon(
+                isActive ? activeIcon : icon,
+                key: ValueKey(isActive),
+                color: isActive ? AppTheme.accent : AppTheme.textMuted,
+                size: 24,
+              ),
+            ),
+            const SizedBox(height: 3),
+            Text(
+              label,
+              style: TextStyle(
+                fontSize: 11,
+                color: isActive ? AppTheme.accent : AppTheme.textMuted,
+                fontWeight: isActive ? FontWeight.w600 : FontWeight.w400,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
